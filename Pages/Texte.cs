d5ddde63835dec08835bbbee4ff3d0fa3dcdf29b
whis2401 <samleafs@protonmail.com>
2018-06-16 08:26:56 -0400

    Ne pas déplacer le curseur x quand on ajoute du texte à une image

--- Pages/Texte.cs
+++ Pages/Texte.cs
@@ -1,17 +1,26 @@
-﻿using System;
+﻿using iTextSharp.text;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Xml;
 
 namespace Pages
 {
     class Texte : Element
     {
+        private float scale;
+
         public Texte(XmlNode element) : base(element)
         {
-            
+            this.scale = float.Parse(element.Attributes["scale"]?.InnerText);
+        }
+
+        public override void Redimensionner(float hauteurCase)
+        {
+            this.image.ScalePercent(this.scale);
         }
     }
 }
