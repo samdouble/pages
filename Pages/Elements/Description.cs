c67515ad6f2f3560b41f1a7c3daad04da0722401
samdouble <samleafs@protonmail.com>
2019-11-26 22:33:20 -0500

    Image is a Panel's attribute instead of being one of its Elements

--- Pages/Elements/Description.cs
+++ Pages/Elements/Description.cs
@@ -5,52 +5,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows;
 using System.Xml;
 
 namespace Pages
 {
-    public class Image
+    class Description : Element
     {
-        private iTextSharp.text.Image image;
+        private string text;
 
-        public Image(string src)
+        public Description(XmlNode element) : base(element)
         {
-            this.image = iTextSharp.text.Image.GetInstance(src);
+            this.text = element.Attributes["text"]?.InnerText;
         }
 
-        public iTextSharp.text.Image GetImage()
-        {
-            return this.image;
-        }
-
-        public void SetHeight(float height)
+        public override void SetHeight(float height)
         {
             float pctScaling = height / this.image.Height;
             this.image.ScalePercent(100 * pctScaling);
         }
 
-        public float getHauteur()
-        {
-            return this.image.ScaledHeight;
-        }
-
-        public float getLargeur()
-        {
-            return this.image.ScaledWidth;
-        }
-
-        public void Decouper(PdfWriter procEcriture, float decoupageGauche, float offset)
+        public override void Decouper(PdfWriter procEcriture, float decoupageGauche, float offset)
         {
             this.image = Crop(this.image, procEcriture, decoupageGauche, 0, this.image.ScaledWidth - offset, this.image.ScaledHeight);
         }
-        public void Positionner(Document doc, float x, float y, float margeHaut, float margeGauche)
-        {
-            Point position = new Point(margeGauche + x, doc.PageSize.Height - margeHaut - y - this.image.ScaledHeight);
-            this.image.SetAbsolutePosition((float) position.X, (float) position.Y);
-        }
 
-        public void AjouterBordures()
+        public override void AjouterBordures()
         {
             this.image.Border = Rectangle.BOX;
             this.image.BorderColor = BaseColor.BLACK;
