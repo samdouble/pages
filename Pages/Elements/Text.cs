c6a6836b6aa951934a6edb5f0804b1984322926f
samdouble <samleafs@protonmail.com>
2020-02-22 23:33:09 -0500

    Place more than one TextElement in a Panel with the top, left and width attributes

--- Pages/Elements/Text.cs
+++ Pages/Elements/Text.cs
@@ -11,14 +11,24 @@ namespace Pages.Elements
 {
     class Text : Element
     {
+        protected Panel parent;
         protected string text;
         protected const int FONT_SIZE = 15;
-        protected const int LINE_HEIGHT = 16;
+        protected const int LINE_HEIGHT = 14;
+        protected const int MARGIN = 5;
         protected Font font;
+        protected float left = 0;
+        protected float top = 0;
+        protected float? width;
 
-        public Text(XmlNode element) : base(element)
+        public Text(XmlNode element, Panel parent) : base(element)
         {
+            this.parent = parent;
             this.text = element.Attributes["text"]?.InnerText;
+            // Optional
+            this.left = element.Attributes["left"] != null ? float.Parse(element.Attributes["left"].InnerText) : 0.0f;
+            this.top = element.Attributes["top"] != null ? float.Parse(element.Attributes["top"].InnerText) : 0.0f;
+            this.width = element.Attributes["width"] != null ? float.Parse(element.Attributes["width"].InnerText) : (float?) null;
             // Load Font
             BaseFont customfont = BaseFont.CreateFont(@"../../Comicsam-Bold.ttf", BaseFont.CP1252, BaseFont.EMBEDDED);
             this.font = new Font(customfont, FONT_SIZE);
@@ -26,10 +36,20 @@ namespace Pages.Elements
 
         public override void Render(Document doc, PdfWriter writer)
         {
+            float left = this.parent.getPosition().X + this.left + MARGIN;
+            float right;
+            if (this.width is float width) {
+                right = this.parent.getPosition().X + this.left + Math.Min(width, this.parent.getLargeur() - this.left) - MARGIN;
+            } else {
+                right = this.parent.getPosition().X + this.parent.getLargeur() - MARGIN;
+            }
+            
+            float top = this.parent.getPosition().Y - this.top;
+            float bottom = this.parent.getPosition().Y - this.parent.getHauteur() + MARGIN;
             PdfContentByte cb = writer.DirectContent;
             ColumnText ct = new ColumnText(cb);
             Paragraph phrase = new Paragraph(this.text, this.font);
-            ct.SetSimpleColumn(phrase, 30, 780, 250, 600, LINE_HEIGHT, 0);
+            ct.SetSimpleColumn(phrase, left, top, right, bottom, LINE_HEIGHT, 0);
             ct.Go();
         }
     }
