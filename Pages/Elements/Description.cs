8e869aa2ea232f4c4ff8628c5bfa4a3fce1897ed
samdouble <samleafs@protonmail.com>
2020-04-11 14:53:16 -0400

    Added visible property to hide descriptions

--- Pages/Elements/Description.cs
+++ Pages/Elements/Description.cs
@@ -12,9 +12,20 @@ namespace Pages
 {
     class Description : Text
     {
+        protected bool visible = true;
+
         public Description(XmlNode element, Panel parent) : base(element, parent)
         {
+            this.visible = element.Attributes["visible"] != null ? bool.Parse(element.Attributes["visible"].InnerText) : true;
             this.font.SetColor(255, 0, 0);
+        }
+
+        public override void Render(Document doc, PdfWriter writer)
+        {
+            if (this.visible)
+            {
+                base.Render(doc, writer);
+            }
         }
     }
 }
