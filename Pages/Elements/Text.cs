d495aabfe46f2079703bdb2055bd59ffeedc6987
samdouble <samleafs@protonmail.com>
2022-08-18 10:49:00 -0400

    Comicsam-Bold.ttf

--- Pages/Elements/Text.cs
+++ Pages/Elements/Text.cs
@@ -30,7 +30,7 @@ namespace Pages.Elements
             this.top = element.Attributes["top"] != null ? float.Parse(element.Attributes["top"].InnerText) : 0.0f;
             this.width = element.Attributes["width"] != null ? float.Parse(element.Attributes["width"].InnerText) : (float?) null;
             // Load Font
-            BaseFont customfont = BaseFont.CreateFont(@"Comicsam-Bold.ttf", BaseFont.CP1252, BaseFont.EMBEDDED);
+            BaseFont customfont = BaseFont.CreateFont("Comicsam-Bold.ttf", BaseFont.CP1252, BaseFont.EMBEDDED);
             this.font = new Font(customfont, FONT_SIZE);
         }
 
